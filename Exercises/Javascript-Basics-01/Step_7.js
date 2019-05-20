7caae306e1f33d0f6b021a88d62d08cd2a033811
Maissaa <maissaa.sh@gmail.com>
2019-05-20 18:34:59 +0300

    Step 7 : Multiple operations

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,6 @@
+function myfunction(){ 
+    var shoe=parseInt(document.getElementById("shoe_size").value);
+    var birth=parseInt(document.getElementById("year").value); 
+var result=(shoe*2)+5*50-birth+1766;
+alert(result);
+}
