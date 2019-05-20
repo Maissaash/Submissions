00a65c8ea21b0e113b2b302fc820b473ddf38073
Maissaa <maissaa.sh@gmail.com>
2019-05-20 18:37:31 +0300

    Step 8 : if, else

---
+++ Exercises/Javascript-Basics-01/Step_8.js
@@ -0,0 +1,12 @@
+
+function myfunction(){ 
+    var age=parseInt(document.getElementById("age").value);
+    
+if(age>=18){
+    alert("you are over 18")
+}
+else{
+    alert( "you are under 18")
+}
+
+}
