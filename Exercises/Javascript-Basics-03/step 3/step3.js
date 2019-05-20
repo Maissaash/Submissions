17e6319d0f8105ed4873b6444079291a663294a2
Maissaa <maissaa.sh@gmail.com>
2019-05-20 21:48:19 +0300

    Step 3

---
+++ Exercises/Javascript-Basics-03/step 3/step3.js
@@ -0,0 +1,9 @@
+function gcolor() {
+    document.querySelector('p').style.color="green";
+  }
+  function rcolor() {
+    document.querySelector('p').style.color="red";
+  }
+  function bcolor() {
+    document.querySelector('p').style.color="blue";
+  }
