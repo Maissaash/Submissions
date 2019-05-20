9173641ec2ae2f3ea8db8456cfbb6bb9de5ffcb8
Maissaa <maissaa.sh@gmail.com>
2019-05-20 18:28:01 +0300

    Step 5 : Simple Math

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,5 @@
+function myfunction(){ 
+    var first=parseInt(document.getElementById("first_number").value);
+    var second=parseInt(document.getElementById("second_number").value);
+      alert(first*second)
+}     
