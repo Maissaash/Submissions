136c4ebdd8a09c1db59ce4069d695381571c8ff1
Maissaa <maissaa.sh@gmail.com>
2019-05-20 18:20:06 +0300

    Step 4:Strings concatenation2

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,8 @@
+
+function myfunction(){
+    var name=document.getElementById("name").value;
+    var surname=document.getElementById("surname").value;
+    var city=document.getElementById("city").value;
+
+       alert("nom :"+" "+name +"\n\r" +"prenom :" +" "+surname + "\n\r" +"ville :"+" "+city);
+}
