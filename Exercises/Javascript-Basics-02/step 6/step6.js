56b74830560d87a8a7448ad7becb52e90216a052
Maissaa <maissaa.sh@gmail.com>
2019-05-20 21:32:33 +0300

    Step 6

---
+++ Exercises/Javascript-Basics-02/step 6/step6.js
@@ -0,0 +1,40 @@
+function setnewimage(s){
+   
+    if(s=="image1"){
+         document.images["image1"].src="images/image1_2.jpg";  
+      }
+    else if(s=="image2"){
+      document.images["image2"].src="images/image2_2.jpg";
+      }
+    else if(s=="image3"){
+      document.images["image3"].src="images/image3_2.jpg";
+      }
+    else if(s=="image4"){
+      document.images["image4"].src="images/image4_2.jpg";
+      }
+    else if(s=="image5"){
+      document.images["image5"].src="images/image5_2.jpg";  
+      }
+  
+    }
+
+
+function oldimage(s){
+    if(s=="image1"){
+        document.images["image1"].src="images/image1.jpg";  
+     }
+   else if(s=="image2"){
+     document.images["image2"].src="images/image2.jpg";
+     }
+   else if(s=="image3"){
+     document.images["image3"].src="images/image3.jpg";
+     }
+   else if(s=="image4"){
+     document.images["image4"].src="images/image4.jpg";
+     }
+   else if(s=="image5"){
+     document.images["image5"].src="images/image5.jpg";  
+     }
+ 
+   }
+
