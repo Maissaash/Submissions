5db4340a9f6e179495796d83c0966c7b11438625
Maissaa <maissaa.sh@gmail.com>
2019-05-20 18:05:49 +0300

    Step2:Variables 2

---
+++ Exercises/Javascript-Basics-01/Step_2.js
@@ -0,0 +1,6 @@
+
+         var name="Yara"
+         var surname="saly"
+         var city="Beirut"
+         alert("nom :"+" "+name +"\n\r" +"prenom :" +" "+surname + "\n\r" +"ville :"+" "+city);
+        
