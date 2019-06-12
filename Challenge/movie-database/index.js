58ecf238507584e4966efb080ed2e77821d41d3c
Maissaa <maissaa.sh@gmail.com>
2019-06-12 12:59:38 +0300

    Step 4 - Let's complicate the API

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,12 +3,27 @@ var express = require('express')
      app.get('/', (req, res) => { 
          res.send('ok')    
      });
+     /* test */
     app.get('/test', (req, res) => { 
         res.send('{status:200, message:"ok"}')
-    });    
+    });  
+    /* get the time */  
     app.get('/time', (req, res) => { 
         var today = new Date(); 
         var time = today.getHours() +":" + today.getSeconds();
         res.send({status:200,message:time})
-    });   
+    }); 
+    /* hello with id */ 
+    app.get('/hello/:ID', (req, res) => { 
+        res.send({status:200, message:`hello ${req.params.ID}`})
+    }); 
+    /* search */
+    app.get('/search', (req, res) => { 
+        if(req.query.s !==""){
+            res.send({status:200, message:"ok", data:req.query.s}) 
+        }
+        else{
+            res.send({status:500, error:true, message:"you have to provide a search"})  
+        }
+    });
 app.listen(3000, () => console.log('listinig on port 3000'));
